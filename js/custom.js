e3679bb437a3e7e0b2598d5d2e9c287621420ac7
Max Christensen <max@Maxs-MacBook-Pro.local>
2022-11-29 16:02:20 +1300

    form inputs filling results and more information on click is functional

--- js/custom.js
+++ js/custom.js
@@ -10,7 +10,7 @@ var cars = [
     // motorbike
     {
         name: "Harley", 
-        image: "img/img1.jpg",
+        image: "img/harleyBike.jpg",
         makeModel: "1986 Harley Davidson Street 500",
         minDays: "1",
         maxDays: "5",
@@ -18,10 +18,11 @@ var cars = [
         maxPassengers: "1",
         costPerDay: "$109",
         litresPer100km: "3.7",
+        wheelchairFriendly: false,
     },
     {
         name: "Honda",
-        image: "img/img1.jpg",
+        image: "img/hondaBike.jpg",
         makeModel: "1969 Honda CB750",
         minDays: "1",
         maxDays: "5",
@@ -29,6 +30,7 @@ var cars = [
         maxPassengers: "1",
         costPerDay: "$109",
         litresPer100km: "3.7",
+        wheelchairFriendly: false,
     },
     // small cars
     {
@@ -41,10 +43,11 @@ var cars = [
         maxPassengers: "2",
         costPerDay: "$129",
         litresPer100km: "8.5",
+        wheelchairFriendly: false,
     },
     {
         name: "Gangster Special",
-        image: "img/img2.jpg",
+        image: "img/gangsterCar.jpg",
         makeModel: "1950 Cadillac Sixty Special",
         minDays: "1",
         maxDays: "10",
@@ -52,10 +55,11 @@ var cars = [
         maxPassengers: "2",
         costPerDay: "$129",
         litresPer100km: "8.5",
+        wheelchairFriendly: true,
     },
     {
         name: "Beetle",
-        image: "img/img2.jpg",
+        image: "img/volkswagenCar.jpg",
         makeModel: "1954 Volkswagen Beetle",
         minDays: "1",
         maxDays: "10",
@@ -63,11 +67,12 @@ var cars = [
         maxPassengers: "2",
         costPerDay: "$129",
         litresPer100km: "8.5",
+        wheelchairFriendly: true,
     },
     // large cars
     {
         name: "Hippie Van",
-        image: "img/img2.jpg",
+        image: "img/volkswagenBus.jpg",
         makeModel: "1980 Volkswagen Kombi",
         minDays: "3",
         maxDays: "10",
@@ -75,10 +80,11 @@ var cars = [
         maxPassengers: "5",
         costPerDay: "$144",
         litresPer100km: "9.7",
+        wheelchairFriendly: true,
     },
     {
         name: "Defender",
-        image: "img/img2.jpg",
+        image: "img/landRoverCar.jpg",
         makeModel: "1986 Land Rover Defender",
         minDays: "3",
         maxDays: "10",
@@ -86,11 +92,12 @@ var cars = [
         maxPassengers: "5",
         costPerDay: "$144",
         litresPer100km: "8.7",
+        wheelchairFriendly: true,
     },
     // motor homes
     {
         name: "Airstream",
-        image: "img/img3.jpg",
+        image: "img/airstreamMotorHome.jpg",
         makeModel: "1993 Airstream Globetrotter",
         minDays: "2",
         maxDays: "15",
@@ -98,10 +105,12 @@ var cars = [
         maxPassengers: "6",
         costPerDay: "$200",
         litresPer100km: "17",
+        wheelchairFriendly: true,
+
     },
     {
         name: "Coachmen",
-        image: "img/img3.jpg",
+        image: "img/coachmenMotorHome.jpg",
         makeModel: "1978 Dodge Coachmen",
         minDays: "2",
         maxDays: "15",
@@ -109,6 +118,7 @@ var cars = [
         maxPassengers: "6",
         costPerDay: "$200",
         litresPer100km: "17",
+        wheelchairFriendly: true,
     },
 
 ];
@@ -117,8 +127,11 @@ var cars = [
 var fromdate;
 var todate;
 var difference;
+var passengers;
 var days;
 var tickbox = $("#wheelchair");
+var carResults = $('.results');
+var moreCarInfo = $('.moreInformation');
 
 // form validation
 var parsleyForm = $("#parsleyValidation").parsley();
@@ -128,16 +141,18 @@ $('#parsleyValidation').find('#searchResultsButton').click(function(event){
 });
 
 parsleyForm.subscribe('parsley:form:success', function() {
-    var fromdate = $("#frompicker").datepicker("getDate");
-    var todate = $("#topicker").datepicker("getDate");
-    var difference = fromdate - todate;
-    var days = Math.abs(Math.ceil(difference / (1000 * (60 * 60) * 24)));
+    fromdate = $("#frompicker").datepicker("getDate");
+    todate = $("#topicker").datepicker("getDate");
+    difference = fromdate - todate;
+    days = Math.abs(Math.ceil(difference / (1000 * (60 * 60) * 24)));
+    passengers = $('#passengerpicker').val();
     console.log('great success, very nice!');
     $('.featuredCars').hide();
-
+    $('.resultsSection').css('visibility', 'visible');
+    $('.fuelCosts').css('visibility', 'visible');
+    showCarResults();
 });
 
-// Assign DatePickers
 $(function(){
     $("#frompicker").datepicker({
         dateFormat: "dd-mm-yy"
@@ -146,6 +161,7 @@ $(function(){
         dateFormat: "dd-mm-yy"
     });
 });
+
 
 /** function to show results in the DOM based on inputed information into the form
     * run a for loop through my cars array
@@ -156,20 +172,45 @@ $(function(){
 */
 
 function showCarResults(){
-    for (let i = 0; i < cars.length; i++) {
+    console.log("this is working :)");
+    var resultsOutput = $('.searchResults');
+    resultsOutput.html(' ');
+    for (var i = 0; i < cars.length; i++) {
         var car = cars[i];
-        if (days >= car.minDays && days <= car.maxDays) {
-            return;            
-        };
-        if (passengers >= car.minPassengers && days <= car.maxPassengers) {
-            return;            
+        if ((days > car.minDays && days < car.maxDays) && (passengers > car.minPassengers && passengers < car.maxPassengers)) {
+            resultsOutput.append(`
+            <div class="resultItem">
+                <div class="resultImg">
+                    <img class="previewImg" src="${car.image}" alt="">
+                </div>
+                <div class="resultInfo">
+                    <h3>${car.name}</h3>
+                    <h6>${car.makeModel}</h6>
+                    <p>${car.litresPer100km}/100km</p>
+                </div>
+                <div class="priceTag">
+                    <h4>${car.costPerDay}<br>/Day</h4>
+                </div>
+            </div>
+            `);
+        showMoreInformation();
+        } else {
+
         }
-        if (tickbox.checked == true) {
-            return;
-        }
+
         
-    }
+        }
 }
 
-
+// onclick for more information visibility
+function showMoreInformation() {
+    var resultItems = document.querySelectorAll('.resultItem');
+    Array.from(resultItems).forEach(function(resultItems){
+        resultItems.addEventListener('click', function() {
+            $('.moreInformation').css('visibility', 'visible');
+        });
+    });
+}
+// end of jQuery master function
 });
+
